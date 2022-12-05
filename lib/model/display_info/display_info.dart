f4968c9e5dc9efa9fcaa5c0b7c837bf96df82662
bowon lee <asdfg3678@gmail.com>
2022-12-05 22:27:33 +0900

    확장함수 위치 변경

--- lib/model/display_info/display_info.dart
+++ lib/model/display_info/display_info.dart
@@ -12,3 +12,23 @@ class DisplayInfo with _$DisplayInfo {
       required int remaining,
       required List<Holiday> closeHoliday}) = _DisplayInfo;
 }
+
+extension DisplayInfoExtension on List<Holiday> {
+  toDisplayInfo(String year, List<Holiday> origin) {
+    List<Holiday> withoutWeekend = origin.where((element) {
+      int weekday = DateTime.parse(element.date).weekday;
+      return weekday != 6 && weekday != 7;
+    }).toList();
+
+    List<Holiday> remaining = withoutWeekend
+        .where(
+            (element) => DateTime.parse(element.date).isAfter(DateTime.now()))
+        .toList();
+
+    return DisplayInfo(
+        year: year,
+        totalCount: withoutWeekend.length,
+        remaining: remaining.length,
+        closeHoliday: remaining);
+  }
+}
--- lib/model/holiday/holiday.dart
+++ lib/model/holiday/holiday.dart
@@ -1,7 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-import '../display_info/display_info.dart';
-
 /// date : "2022-01-01"
 /// dateName : "1월1일"
 /// dateKind : "국경일"
@@ -19,24 +17,4 @@ class Holiday with _$Holiday {
 
   factory Holiday.fromJson(Map<String, dynamic> json) =>
       _$HolidayFromJson(json);
-}
-
-extension DisplayInfoExtension on List<Holiday> {
-  toDisplayInfo(String year, List<Holiday> origin) {
-    List<Holiday> withoutWeekend = origin.where((element) {
-      int weekday = DateTime.parse(element.date).weekday;
-      return weekday != 6 && weekday != 7;
-    }).toList();
-
-    List<Holiday> remaining = withoutWeekend
-        .where(
-            (element) => DateTime.parse(element.date).isAfter(DateTime.now()))
-        .toList();
-
-    return DisplayInfo(
-        year: year,
-        totalCount: withoutWeekend.length,
-        remaining: remaining.length,
-        closeHoliday: remaining);
-  }
 }
