295bfc89c318d946a607d5c18334f8532687d7be
bowon lee <asdfg3678@gmail.com>
2023-01-16 23:34:33 +0900

    holidayList 확장함수 정의 및 테스트 케이스 작성

---
+++ lib/model/holiday/holiday_extention.dart
@@ -0,0 +1,84 @@
+import 'holiday.dart';
+
+/// list를 사용하여 표현되는 타입
+/// 데이터 타입에 종속적이지 않는건 장점이다.
+/// 변경에 용이하나 강제적이지 않다
+/// 주말을 포함 한 모든 휴일 수
+/// 주말을 제외한 모든 휴일 수
+/// 주말로 인해 날아간 휴일 수
+/// 대체공휴일 수
+/// 남은 휴일 수
+/// 가장 가까운 휴일
+/// 휴일 목록 월별, 연도별
+extension HolidayListDivideExtension on List<Holiday> {
+  /// 휴일 목록 연도별 YYYY
+  Map<int, List<Holiday>> divideByYear() {
+    Map<int, List<Holiday>> result = {};
+
+    forEach((element) {
+      int year = DateTime.parse(element.date).year;
+      if (result.containsKey(year)) {
+        result[year]?.add(element);
+      } else {
+        result[year] = [element];
+      }
+    });
+
+    return result;
+  }
+
+  /// 휴일 목록 월별 MM
+  Map<int, List<Holiday>> divideByMonth() {
+    Map<int, List<Holiday>> result = {};
+
+    forEach((element) {
+      int month = DateTime.parse(element.date).month;
+      if (result.containsKey(month)) {
+        result[month]?.add(element);
+      } else {
+        result[month] = [element];
+      }
+    });
+
+    return result;
+  }
+
+  /// 휴일 목록 연도 + 월 YYYYMM
+  Map<int, List<Holiday>> divideByYearAndMonth() {
+    Map<int, List<Holiday>> result = {};
+
+    forEach((element) {
+      int year = DateTime.parse(element.date).year;
+      int month = DateTime.parse(element.date).month;
+
+      int format = year * 100 + month;
+      if (result.containsKey(format)) {
+        result[format]?.add(element);
+      } else {
+        result[format] = [element];
+      }
+    });
+
+    return result;
+  }
+
+  /// 주말을 제외한 모든 휴일
+  List<Holiday> getWithoutWeekend() {
+    return where((element) {
+      return DateTime.parse(element.date).weekday != 6 &&
+          DateTime.parse(element.date).weekday != 7;
+    }).toList();
+  }
+
+  /// 주말로 인해 날아간 휴일 수
+  /// 대체공휴일 수
+  /// 남은 휴일 수
+  List<Holiday> getRemainingList() {
+    return where((element) {
+      return DateTime.parse(element.date).isAfter(DateTime.now());
+    }).toList();
+  }
+
+  /// 가장 가까운 휴일
+
+}
---
+++ test/holiday_extention_test.dart
@@ -0,0 +1,50 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:holiday/model/holiday/holiday.dart';
+import 'package:holiday/model/holiday/holiday_extention.dart';
+
+void main() {
+  List<Holiday> mockList = [
+    Holiday(
+        date: "2023-01-20", dateName: "설날", dateKind: "공휴일", isHoliday: true),
+    Holiday(
+        date: "2023-01-21", dateName: "설날", dateKind: "공휴일", isHoliday: true),
+    Holiday(
+        date: "2023-01-22", dateName: "설날", dateKind: "공휴일", isHoliday: true),
+    Holiday(
+        date: "2023-01-23", dateName: "설날", dateKind: "공휴일", isHoliday: true),
+    Holiday(
+        date: "2023-03-01", dateName: "삼일절", dateKind: "공휴일", isHoliday: true),
+    Holiday(
+        date: "2024-03-01", dateName: "삼일절", dateKind: "공휴일", isHoliday: true),
+  ];
+
+  test("divide_year", () {
+    var yearMap = mockList.divideByYear();
+    expect(2023, yearMap.keys.first);
+    expect(2024, yearMap.keys.last);
+  });
+
+  test("divide_mon", () {
+    var monthMap = mockList.divideByMonth();
+    expect(1, monthMap.keys.first);
+    expect(3, monthMap.keys.last);
+  });
+
+  test("divide_year_mon", () {
+    var monthMap = mockList.divideByYearAndMonth();
+    expect(202301, monthMap.keys.first);
+    expect(202403, monthMap.keys.last);
+  });
+
+  test("weekend", () {
+    var withOutWeekend =
+        mockList.divideByYearAndMonth()[202301]?.getWithoutWeekend();
+    expect(2, withOutWeekend?.length);
+  });
+
+  test("remaining", () {
+    var withOutWeekend =
+        mockList.divideByYearAndMonth()[202301]?.getRemainingList();
+    expect(4, withOutWeekend?.length);
+  });
+}
