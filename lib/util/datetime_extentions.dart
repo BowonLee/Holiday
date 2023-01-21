698855129d727eb98a609340279f5017fe69ab03
bowon lee <asdfg3678@gmail.com>
2023-01-21 13:20:34 +0900

    datetime 상태 변환 추가

---
+++ lib/util/datetime_extentions.dart
@@ -0,0 +1,27 @@
+extension DateTimeExtention on DateTime {
+  /// past 1 , now 0, future -1
+  int compareDateString(String date) {
+    return DateTime.parse(date).compareTo(getNowDate());
+  }
+
+  DateTime getNowDate() {
+    var now = DateTime.now();
+    return DateTime(now.year, now.month, now.day);
+  }
+
+  /// 현제 시간과 비교하여 상태 반환
+  DateState getDateState(DateTime dateTime) {
+    switch (compareTo(dateTime)) {
+      case -1:
+        return DateState.before;
+      case 0:
+        return DateState.now;
+      case 1:
+        return DateState.after;
+      default:
+        return DateState.none;
+    }
+  }
+}
+
+enum DateState { before, now, after, none }
