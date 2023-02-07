3e049567f866ade27376cee5aabc2644744d452f
bowon lee <asdfg3678@gmail.com>
2023-02-07 22:10:12 +0900

    휴일 정보표시 카드 변경

--- lib/layout/component/holiday_info_component.dart
+++ lib/layout/component/holiday_info_component.dart
@@ -4,45 +4,72 @@ import 'package:holiday/model/holiday/holiday_extention.dart';
 
 class HolidayInfoComponent extends StatelessWidget {
   final List<Holiday> holidayList;
-  const HolidayInfoComponent({Key? key, required this.holidayList})
+  final int year;
+  const HolidayInfoComponent({Key? key, required this.holidayList, required this.year})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final TextStyle contentTextStyle = Theme.of(context).textTheme.bodyMedium!.copyWith(
+      fontWeight: FontWeight.bold
+    );
+
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                const Text("총 휴일 수 : "),
-                Text(
-                  holidayList.length.toString(),
-                )
-              ],
-            ),
-            Row(
-              children: [
-                const Text("주말을 제외한 휴일 수 : "),
-                Text(holidayList.toWithoutWeekend().length.toString())
-              ],
-            ),
-            Row(
-              children: [
-                const Text("남은 휴일 수 : "),
-                Text(holidayList
-                    .toWithoutWeekend()
-                    .toRemainingList()
-                    .length
-                    .toString())
-              ],
-            ),
-          ],
+      elevation: 0,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.outline,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(5)),
+        ),
+      child: Container(
+        // decoration: buildBoxDecoration(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text("$year", style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20
+                  )),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("총 휴일 수 : ", style: contentTextStyle,),
+                  Text(
+                    holidayList.length.toString(),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Text("주말을 제외한 휴일 수 : ", style: contentTextStyle,),
+                  Text(holidayList.toWithoutWeekend().length.toString())
+                ],
+              ),
+              Row(
+                children: [
+                  Text("남은 휴일 수 : ", style: contentTextStyle,),
+                  Text(holidayList
+                      .toWithoutWeekend()
+                      .toRemainingList()
+                      .length
+                      .toString())
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+
+
+
 }
 
 ///
