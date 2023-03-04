8f7ac6b4cac2edc44274575ee0c7cbd5bff9ce99
bowon lee <asdfg3678@gmail.com>
2023-03-04 13:28:21 +0900

    계절별 테마 파일에 이모지 및 배경색 정보 추가

--- lib/model/ui/custom_theme_model.dart
+++ lib/model/ui/custom_theme_model.dart
@@ -4,13 +4,15 @@ class CustomThemeModel {
   final String name;
   final String emoji;
   final Color backgroundColor;
-  final ThemeData? themeLightData;
-  final ThemeData? themeDarkData;
+  final ThemeData themeLightData;
+  final ThemeData themeDarkData;
+  final String? assetFilename;
 
   CustomThemeModel(
       {required this.name,
       required this.emoji,
       required this.backgroundColor,
-      this.themeLightData,
-      this.themeDarkData});
+      required this.themeLightData,
+      required this.themeDarkData,
+      this.assetFilename});
 }
--- lib/theme/seasons.dart
+++ lib/theme/seasons.dart
@@ -2,34 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:holiday/model/ui/custom_theme_model.dart';
 import 'package:holiday/theme/flex_theme.dart';
 
+// runner
+final CustomThemeModel springTheme = CustomThemeModel(
+    name: "spring",
+    emoji: "\u{1F3C3}",
+    backgroundColor: Colors.green,
+    themeLightData: dellGenoaGreenLight,
+    themeDarkData: dellGenoaGreenDark,
+    assetFilename: "spring.gif");
+
+// surfer
+final CustomThemeModel summerTheme = CustomThemeModel(
+    name: "summer",
+    emoji: "\u{1F3C4}",
+    backgroundColor: Colors.blue,
+    themeLightData: aquaBlueLight,
+    themeDarkData: aquaBlueDark,
+    assetFilename: "summer_pool.gif");
+
+// leaf
+final CustomThemeModel autumnTheme = CustomThemeModel(
+    name: "autumn",
+    emoji: "\u{1F342}",
+    backgroundColor: Colors.redAccent,
+    themeLightData: redWineThemeLight,
+    themeDarkData: redWineThemeDark,
+    assetFilename: "autumn_dot.gif");
+
 // snowboard
 final CustomThemeModel winterTheme = CustomThemeModel(
     name: "winter",
-    emoji: "1F3C2",
-    backgroundColor: Colors.blue,
-    themeDarkData: midNightThemeLight,
-    themeLightData: midNightThemeDark);
-
-// runner
-final springTheme = CustomThemeModel(
-    name: "spring",
-    emoji: "1F3C3",
-    backgroundColor: Colors.green,
-    themeLightData: dellGenoaGreenLight,
-    themeDarkData: dellGenoaGreenDark);
-
-// surfer
-final summerTheme = CustomThemeModel(
-    name: "summer",
-    emoji: "1F3C4",
-    backgroundColor: Colors.blue,
-    themeDarkData: aquaBlueLight,
-    themeLightData: aquaBlueDark);
-
-// leaf
-final autumnTheme = CustomThemeModel(
-    name: "autumn",
-    emoji: "1F342",
-    backgroundColor: Colors.redAccent,
-    themeLightData: redWineThemeLight,
-    themeDarkData: redWineThemeDark);
+    emoji: "\u{1F3C2}",
+    backgroundColor: Colors.white,
+    themeLightData: midNightThemeLight,
+    themeDarkData: midNightThemeDark,
+    assetFilename: "winter_snow.gif");
