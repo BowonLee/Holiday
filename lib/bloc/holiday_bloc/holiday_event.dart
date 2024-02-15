a57f46b8faa57ad8cc5ca5dabe7eee127151f661
bowon lee <asdfg3678@gmail.com>
2024-02-15 14:05:42 +0700

    휴일정보 업데이트 이벤트 추가

--- lib/bloc/holiday_bloc/holiday_event.dart
+++ lib/bloc/holiday_bloc/holiday_event.dart
@@ -8,3 +8,9 @@ class GetHolidayEvent extends HolidayEvent {
   @override
   List<Object> get props => [];
 }
+
+/// 서버로부터 신규 데이터를 업데이트 한다.
+class UpdateHolidayEvent extends HolidayEvent {
+  @override
+  List<Object> get props => [];
+}
