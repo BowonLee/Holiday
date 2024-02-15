a87433abe3de5d3ad00ba804e55da7ea0b3291ab
bowon lee <asdfg3678@gmail.com>
2024-02-15 14:06:35 +0700

    휴일정보 업데이트 로직 추가

--- lib/bloc/holiday_bloc/holiday_bloc.dart
+++ lib/bloc/holiday_bloc/holiday_bloc.dart
@@ -1,29 +1,42 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:holiday/model/event_date/event_date_extension.dart';
-import 'package:holiday/model/holiday/holiday_extention.dart';
 import 'package:logger/logger.dart';
 
 import '../../repository/holiday_repository.dart';
+
 import 'holiday_event.dart';
 import 'holiday_state.dart';
 
-//
-class HolidayBloc extends Bloc<HolidayEvent, GetHolidayState> {
-  final HolidayRepository repository;
+class HolidayBloc extends Bloc<HolidayEvent, HolidayBlocState> {
+  final HolidayRepository holidayRepository;
 
-  HolidayBloc({required this.repository}) : super(HolidayEmpty()) {
+  HolidayBloc({required this.holidayRepository}) : super(HolidayEmpty()) {
     on<GetHolidayEvent>(_listHolidayEventListener);
   }
 
-  void _listHolidayEventListener(GetHolidayEvent event, Emitter<GetHolidayState> emitter) async {
+  void _listHolidayEventListener(GetHolidayEvent event, Emitter<HolidayBlocState> emitter) async {
+    emitter(HolidayLoading());
     try {
-      emitter(GetHolidayLoading());
-      final resp = await repository.getHolidayList();
+      final holidayList = holidayRepository.getListFromDatabase();
 
-      emitter(GetHolidayLoaded(holidayList: resp));
+      emitter(HolidayLoaded(holidayList: holidayList));
     } catch (e) {
       Logger().i(e);
-      emitter(GetHolidayError(message: e.toString()));
+      emitter(HolidayError(message: e.toString()));
     }
+  }
+
+  void _getNewListFromServer(GetHolidayEvent event, Emitter<HolidayBlocState> emitter) async {
+    emitter(HolidayLoading());
+
+    try {
+      final response = await holidayRepository.getListFromSever();
+      final DateTime lastUpdateLocal = await holidayRepository.getLastUpdateDatetime();
+
+      if (lastUpdateLocal.isBefore(response.lastUpdateTime)) {
+        holidayRepository.setLastUpdateDate(response.lastUpdateTime);
+        holidayRepository.setList(response.holidayList);
+      }
+      emitter(HolidayLoaded(holidayList: response.holidayList));
+    } catch (e) {}
   }
 }
