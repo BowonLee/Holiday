d3189856cd2cedbb0708c70bf1b9aca2317b2167
bowon lee <asdfg3678@gmail.com>
2024-02-18 16:04:57 +0700

    휴일정보 요청 이벤트명 변경 적용

--- lib/layout/home/view/home_view.dart
+++ lib/layout/home/view/home_view.dart
@@ -9,10 +9,32 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<HolidayBloc, GetHolidayState>(
+    return BlocBuilder<HolidayBloc, HolidayBlocState>(
       builder: (context, state) {
         return Container();
       },
     );
+  }
+}
+
+/// 휴일 까지 몇일 남았는지 출력
+class _RemainingDateField extends StatelessWidget {
+  const _RemainingDateField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+/// 지금 어떤 휴일이며, 언제가지 쉬는지 표시
+
+/// 추가 페이지
+class HomeMenuField extends StatelessWidget {
+  const HomeMenuField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
