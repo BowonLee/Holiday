722b846c891e9e31bc64b284b6b88d5b4658e15e
bowon lee <asdfg3678@gmail.com>
2024-02-20 15:07:45 +0700

    미사용 코드 제거

--- lib/bloc/holiday_cubit/holiday_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'holiday_state.dart';
-
-/// 데이터만을 관리한다.
-class HolidayCubit extends Cubit<HolidayState> {
-  HolidayCubit() : super(HolidayInitial());
-}
--- lib/bloc/holiday_cubit/holiday_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'holiday_cubit.dart';
-
-@immutable
-abstract class HolidayState {}
-
-class HolidayInitial extends HolidayState {}
