4a8b8d7f3fa0bae31c34f426230acaf7342bb6c4
bowon lee <asdfg3678@gmail.com>
2024-02-23 16:18:33 +0700

    앱 시작 시 로직에서 휴일정보 획득 로직 분리

--- lib/layout/app_init/app_init_builder.dart
+++ lib/layout/app_init/app_init_builder.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:holiday/bloc/app_init/app_init_bloc.dart';
+import 'package:holiday/repository/holiday_repository.dart';
+import 'package:holiday/repository/metadata_repository.dart';
 
 class AppInitBuilder extends StatelessWidget {
   const AppInitBuilder({super.key});
 
   @override
   Widget build(BuildContext context) {
+    BlocListener<AppInitBloc, AppInitBlocState>(
+      listener: (BuildContext context, AppInitBlocState state) {
+        if (state is AppInitBlocLoading) {
+          /// 로딩일 경우 indicator 정도만
+        }
+      },
+    );
+
+    // BlocProvider<AppInitBloc>();
+    return BlocProvider(
+      create: (_) =>
+          AppInitBloc(metadataRepository: metadataRepositoryProvider(), holidayRepository: holidayRepositoryProvider()),
+      child: const _AppInitBlocBuilder(),
+    );
+  }
+}
+
+class _AppInitBlocBuilder extends StatelessWidget {
+  const _AppInitBlocBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    BlocProvider.of<AppInitBloc>(context).add(GetMetaDataEvent());
+
     return BlocBuilder<AppInitBloc, AppInitBlocState>(
       builder: (context, state) {
-        if (state is AppInitBlocLoading) {}
-        if (state is AppInitBlocComplete) {}
         return Container();
       },
     );
