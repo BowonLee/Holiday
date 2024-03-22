214736d899cd0f93c37596924efef64842a91bd9
bowon lee <asdfg3678@gmail.com>
2024-03-22 18:55:06 +0700

    홈 화면 이전,다음 휴일 이동 버튼 생성 및 기능 구현

--- lib/layout/home/view/home_view.dart
+++ lib/layout/home/view/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:holiday/bloc/holiday_bloc/holiday_bloc.dart';
+import 'package:holiday/layout/calander/calandar_view.dart';
 import 'package:holiday/layout/component/consecutive_holidays_list.dart';
 import 'package:holiday/layout/component/next_consecutive_holidays.dart';
 import 'package:holiday/layout/yearly/yealy_info_view.dart';
@@ -14,6 +15,7 @@ import 'package:holiday/util/datetime_extentions.dart';
 import 'package:logger/logger.dart';
 
 import '../../../bloc/holiday_bloc/holiday_state.dart';
+import '../../user_vacation/user_vacation.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -28,7 +30,6 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: _currentView(),
       bottomNavigationBar: BottomNavigationBar(
         onTap: _onBottomSheetTapped,
@@ -46,7 +47,7 @@ class _HomeViewState extends State<HomeView> {
   }
 
   _currentView() {
-    const viewList = [YearlyInfoView(), HomeMainView(), Text("2")];
+    const viewList = [YearlyInfoView(), HomeMainView(), UserVacation()];
     return viewList[_selectedIndex];
   }
 
@@ -57,26 +58,52 @@ class _HomeViewState extends State<HomeView> {
   }
 }
 
-class HomeMainView extends StatelessWidget {
+class HomeMainView extends StatefulWidget {
   const HomeMainView({super.key});
 
   @override
+  State<HomeMainView> createState() => _HomeMainViewState();
+}
+
+class _HomeMainViewState extends State<HomeMainView> {
+  int cursor = 0;
+
+  @override
   Widget build(BuildContext context) {
-    final HolidayBlocLoaded loadedState = context.read<HolidayBloc>().state as HolidayBlocLoaded;
+    final List<Holiday> holidayList = (context.read<HolidayBloc>().state as HolidayBlocLoaded).holidayList;
 
-    final remaining =
-        loadedState.holidayList.toWithoutWeekend().toRemainingList().toEventDateList().toConsecutiveHolidaysList();
-
-    Logger().i(remaining);
-    return Column(
-      children: [
-        _DateStateField(),
-        Expanded(
-          child: ConsecutiveHolidaysListComponent(
-            consecutiveHolidaysList: remaining,
+    final List<ConsecutiveHolidays> consecutiveHolidaysList =
+        holidayList.toRemainingList().toEventDateList().toConsecutiveHolidaysList();
+    Logger().i(cursor);
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        children: [
+          _DateStateField(
+            holidayList: holidayList,
+            cursoredConsecutiveHolidays: consecutiveHolidaysList[cursor],
           ),
-        )
-      ],
+          MoveDateButtonFiled(
+            prev: cursor == 0 ? null : consecutiveHolidaysList[cursor - 1],
+            onClickPrevButton: () {
+              if (cursor > 0) {
+                setState(() {
+                  cursor--;
+                });
+              }
+            },
+            next: cursor == consecutiveHolidaysList.length - 1 ? null : consecutiveHolidaysList[cursor + 1],
+            onClickNextButton: () {
+              if (cursor < consecutiveHolidaysList.length - 1) {
+                setState(() {
+                  cursor++;
+                });
+              }
+            },
+          ),
+          HomeOverviewField()
+        ],
+      ),
     );
   }
 }
@@ -85,36 +112,23 @@ class HomeMainView extends StatelessWidget {
 /// 지금 휴일이라면 휴일 안내
 /// 휴일 전이라면 D day 표시
 class _DateStateField extends StatelessWidget {
-  const _DateStateField({super.key});
+  const _DateStateField({super.key, required this.holidayList, required this.cursoredConsecutiveHolidays});
+
+  final List<Holiday> holidayList;
+  final ConsecutiveHolidays cursoredConsecutiveHolidays;
 
   @override
   Widget build(BuildContext context) {
-    final HolidayBlocLoaded loadedState = context.read<HolidayBloc>().state as HolidayBlocLoaded;
-
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: _homeMainScreenByDateState(loadedState.holidayList),
+      child: cursoredConsecutiveHolidays.state == DateState.now
+          ? _CurrentConsecutiveHolidayState(
+              consecutiveHolidays: cursoredConsecutiveHolidays,
+            )
+          : _NextConsecutiveHolidayState(
+              consecutiveHolidays: cursoredConsecutiveHolidays,
+            ),
     );
-  }
-
-  Widget _homeMainScreenByDateState(List<Holiday> holidayList) {
-    final next = holidayList.toWithoutWeekend().toRemainingList().toEventDateList().toConsecutiveHolidaysList().first;
-
-    final current = holidayList
-        .toWithoutWeekend()
-        .toEventDateList()
-        .toConsecutiveHolidaysList()
-        .where((element) => element.state == DateState.now);
-
-    if (current.isEmpty) {
-      return _NextConsecutiveHolidayState(
-        consecutiveHolidays: next,
-      );
-    } else {
-      return _CurrentConsecutiveHolidayState(
-        consecutiveHolidays: current.first,
-      );
-    }
   }
 }
 
@@ -145,14 +159,83 @@ class _NextConsecutiveHolidayState extends StatelessWidget {
   }
 }
 
-/// 지금 어떤 휴일이며, 언제가지 쉬는지 표시
-
-/// 추가 페이지
-class HomeMenuField extends StatelessWidget {
-  const HomeMenuField({super.key});
+/// 지금 어떤 휴일이며, 언제까지 쉬는지 표시
+/// 추가 정보용 페이지
+/// 표시할 정보 - 1. 몇일 쉬는지
+class HomeOverviewField extends StatelessWidget {
+  const HomeOverviewField({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [],
+    );
+  }
+}
+
+/// 일자를 이동할 수 있는 버튼 영역
+/// 처음으로 돌아갈 수 있으며 다음 휴일의 이름도 표시
+class MoveDateButtonFiled extends StatefulWidget {
+  final ConsecutiveHolidays? prev;
+  final ConsecutiveHolidays? next;
+  final VoidCallback onClickPrevButton;
+  final VoidCallback onClickNextButton;
+
+  const MoveDateButtonFiled(
+      {super.key, required this.next, required this.onClickNextButton, this.prev, required this.onClickPrevButton});
+
+  @override
+  State<MoveDateButtonFiled> createState() => _MoveDateButtonFiledState();
+}
+
+class _MoveDateButtonFiledState extends State<MoveDateButtonFiled> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextButton(
+                onPressed: widget.prev == null ? null : widget.onClickPrevButton,
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.keyboard_arrow_left,
+                      size: 30,
+                    ),
+                    Text(
+                      "Prev",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                )),
+            Text(widget.prev == null ? "" : widget.prev!.title)
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextButton(
+                onPressed: widget.next == null ? null : widget.onClickNextButton,
+                child: const Row(
+                  children: [
+                    Text(
+                      "Next",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Icon(
+                      Icons.keyboard_arrow_right,
+                      size: 30,
+                    ),
+                  ],
+                )),
+            Text(widget.next == null ? "" : widget.next!.title)
+          ],
+        ),
+      ],
+    );
   }
 }
