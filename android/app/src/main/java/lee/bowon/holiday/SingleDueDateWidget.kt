aa8a4d60a8964e5b160b8f3882f6b255300c4a1f
bowon lee <asdfg3678@gmail.com>
2024-03-27 22:01:09 +0700

    위젯 구성 정보 송신 및 표시 예시 적용

--- android/app/src/main/java/lee/bowon/holiday/SingleDueDateWidget.kt
+++ android/app/src/main/java/lee/bowon/holiday/SingleDueDateWidget.kt
@@ -4,6 +4,7 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetPlugin
 
 /**
  * Implementation of App Widget functionality.
@@ -12,8 +13,19 @@ class SingleDueDateWidget : AppWidgetProvider() {
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            val widgetData = HomeWidgetPlugin.getData(context)
+            val views = RemoteViews(context.packageName, R.layout.single_due_date_widget).apply {
+
+                val title = widgetData.getString("title", null)
+           
+                setTextViewText(R.id.title, title ?: "No title set")
+
+            }
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
+
+
     }
 
     override fun onEnabled(context: Context) {
@@ -29,7 +41,7 @@ internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManage
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.single_due_date_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    views.setTextViewText(R.id.title, widgetText)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
