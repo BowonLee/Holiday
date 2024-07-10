c79aa85c471938ead66e2492483db7b97a97a175
bowon lee <asdfg3678@gmail.com>
2024-07-10 20:43:39 +0900

    현지화 한국어 기본 언어로 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:holiday/bloc/app_init/app_init_bloc.dart';
 import 'package:holiday/client/holiday_client.dart';
 import 'package:holiday/layout/app_init/app_init_builder.dart';
@@ -8,6 +9,7 @@ import 'package:holiday/repository/holiday_repository.dart';
 
 import 'package:holiday/repository/metadata_repository.dart';
 import 'package:holiday/theme_cubit/theme_cubit.dart';
+import 'package:intl/intl.dart';
 
 import 'client/dio_service.dart';
 import 'client/metadata_client.dart';
@@ -33,7 +35,14 @@ void main() async {
         return MaterialApp(
             theme: state.currentThemeModel.themeLightData,
             darkTheme: state.currentThemeModel.themeDarkData,
-            home: const AppInitBuilder());
+            home: const AppInitBuilder(),
+            localizationsDelegates: const [
+              GlobalMaterialLocalizations.delegate
+            ],
+            supportedLocales: [
+              Locale('en', ''), // English, no country code
+              Locale('ko', ''),
+            ]);
       },
     ),
   ));
@@ -45,10 +54,12 @@ Future<void> preInit() async {
    * 정보 업데이트해야 하는지 확인
    */
 
+  // Intl.defaultLocale = 'ko_KR';
   setupDio();
   setUpHolidayClient();
   setUpMetaClient();
-
+  Intl.defaultLocale = 'ko_KR';
+  // await initializeDateFormatting('ko_KR', null);
   await HiveHelper().initHiveManager();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
