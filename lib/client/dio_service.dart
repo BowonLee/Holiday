8ec6b1577a7583e5b67d7ac6684db7035f2b4559
bowon lee <asdfg3678@gmail.com>
2024-07-12 16:42:41 +0900

    서버 통신 시 타임아웃 시간 설정

--- lib/client/dio_service.dart
+++ lib/client/dio_service.dart
@@ -1,15 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+const int serverTimeoutSecond = 3;
+
 void setupDio() {
   GetIt.instance.registerLazySingleton(() => DioService());
 }
 
 class DioService {
-  final dio = Dio();
-
-  // get method
-  Future<Response> get(String path) async {
-    return await dio.get(path);
-  }
+  final dio = Dio(BaseOptions(connectTimeout: const Duration(seconds: serverTimeoutSecond)));
 }
