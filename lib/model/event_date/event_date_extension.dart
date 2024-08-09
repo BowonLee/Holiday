57f4dae606ba76ad8c6abf9e417f2e0158271b7c
bowon lee <asdfg3678@gmail.com>
2024-08-09 19:06:40 +0900

    연휴생성 로직 리펙토링 일요일부터 시작하는 경우 예외처리 추가

--- lib/model/event_date/event_date_extension.dart
+++ lib/model/event_date/event_date_extension.dart
@@ -7,14 +7,13 @@ extension ConsecutiveHolidaysParseExtention on List<EventDate> {
   List<ConsecutiveHolidays> toConsecutiveHolidaysList() {
     List<ConsecutiveHolidays> result = [];
 
-    /// 휴일은 여기에서 추가합니다. 해당 리스트는 날짜순으로 정렬되어 있다고 가정합니다.
-    /// 1. 휴일 리스트를 순서대로 순회한다.
-    /// 2. 해당일 전일을 검사하여 주말을 추가한다. <- 이건 마지막에 해도 상관없나?
-    /// 3. 해당일 다음날부터 검사하여 다른 휴일 및 주말을 추가한다.
-    /// 4. 다른 휴일이 해당 리스트에 추가되는 경우 리스트에서 제외한다.
-    /// 5. 연속된 휴일이 없는 경우 해당 루프를 종료한 뒤 하나의 연휴로 묶는다.
-    /// 6. 연휴의 이름은 포함 된 공휴일의 이름을 추가한다.
-    /// 7. 연휴가 1일인 경우 휴일의 이음, 2일 이상인 경우 휴일이름 + 연휴 이다.
+    /// 연속된 휴일을 연휴로 묶는다.
+    /// * 휴일 리스트를 인자로 받는다. (확장함수이기에 자기 참조 가능)
+    /// 1. 대상으로 삼는 인자의 날짜를 저장하고 연휴를 생성한다.
+    /// 2. 원소의 일자가 다음날이거나, 주말이면 해당 연휴에 일자를 추가하고 1로 돌아간다
+    /// 3. 원소의 일자가 연휴에 해당하지 않으면 새로운 연휴를 생성한다.
+    ///
+    /// 이 경우 주말부터 시작하는 경우 문제가 될 수 있다.
 
     int cursor = 0;
     while (cursor < length) {
@@ -24,25 +23,29 @@ extension ConsecutiveHolidaysParseExtention on List<EventDate> {
       int add = 1;
       int weekendAdd = 0;
       while (true) {
-        DateTime tempDate =
-            target.datetime.add(Duration(days: add + weekendAdd));
+        DateTime checkedDate = target.datetime.add(Duration(days: add + weekendAdd));
 
-        if (_isContainDate(tempDate)) {
+        if (_isContainDate(checkedDate)) {
           dateList.add(this[cursor + add]);
           add++;
-        } else if (tempDate.isWeekend()) {
-          dateList.add(EventDate.fromWeekendDateTime(tempDate));
+        } else if (checkedDate.isWeekend()) {
+          dateList.add(EventDate.fromWeekendDateTime(checkedDate));
           weekendAdd++;
         } else {
           break;
         }
       }
 
-      if (dateList[0].datetime.weekday == 1) {
-        dateList.add(EventDate.fromWeekendDateTime(
-            dateList[0].datetime.add(const Duration(days: -1))));
-        dateList.add(EventDate.fromWeekendDateTime(
-            dateList[0].datetime.add(const Duration(days: -2))));
+      /// 시작일이 월요일 혹은 일요일인 경우 예외처리 추가
+      final firstDate = dateList[0];
+      if (firstDate.datetime.weekday == DateTime.monday) {
+        dateList
+            .add(EventDate.fromWeekendDateTime(firstDate.datetime.add(const Duration(days: -1))));
+        dateList
+            .add(EventDate.fromWeekendDateTime(firstDate.datetime.add(const Duration(days: -2))));
+      } else if (firstDate.datetime.weekday == DateTime.saturday) {
+        dateList
+            .add(EventDate.fromWeekendDateTime(firstDate.datetime.add(const Duration(days: -1))));
       }
 
       dateList.sort((a, b) => a.datetime.compareByDate(b.datetime));
@@ -54,6 +57,8 @@ extension ConsecutiveHolidaysParseExtention on List<EventDate> {
     return result;
   }
 
+  /// 특정 일자가 해당 배열에 있는지 확인
+  /// 정렬 되지 않은 배열이 들어올 경우 판단을 위해 사용
   bool _isContainDate(DateTime target) {
     return map((e) => e.datetime)
             .toList()
