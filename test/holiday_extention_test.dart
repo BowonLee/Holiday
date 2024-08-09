c7af44df2b3b9294c48206a4b1a672cc0ace6549
bowon lee <asdfg3678@gmail.com>
2024-08-09 19:13:25 +0900

    holiday 타입 확장함수에 대한 테스트 항목 추가

--- test/holiday_extention_test.dart
+++ test/holiday_extention_test.dart
@@ -4,31 +4,12 @@ import 'package:holiday/model/holiday/holiday_extention.dart';
 
 void main() {
   List<Holiday> mockList = [
-    Holiday(
-        date: DateTime(2023, 01, 20),
-        dateName: "설날",
-        dateKind: "공휴일",
-        isHoliday: true),
-    Holiday(
-        date: DateTime(2023, 01, 21),
-        dateName: "설날",
-        dateKind: "공휴일",
-        isHoliday: true),
-    Holiday(
-        date: DateTime(2023, 01, 22),
-        dateName: "설날",
-        dateKind: "공휴일",
-        isHoliday: true),
-    Holiday(
-        date: DateTime(2023, 01, 23),
-        dateName: "설날",
-        dateKind: "공휴일",
-        isHoliday: true),
-    Holiday(
-        date: DateTime(2023, 03, 1),
-        dateName: "삼일절",
-        dateKind: "공휴일",
-        isHoliday: true),
+    Holiday(date: DateTime(2023, 01, 20), dateName: "설날", dateKind: "공휴일", isHoliday: true),
+    Holiday(date: DateTime(2023, 01, 21), dateName: "설날", dateKind: "공휴일", isHoliday: true),
+    Holiday(date: DateTime(2023, 01, 22), dateName: "설날", dateKind: "공휴일", isHoliday: true),
+    Holiday(date: DateTime(2023, 01, 23), dateName: "설날", dateKind: "공휴일", isHoliday: true),
+    Holiday(date: DateTime(2023, 03, 1), dateName: "삼일절", dateKind: "공휴일", isHoliday: true),
+    Holiday(date: DateTime(2024, 03, 1), dateName: "삼일절", dateKind: "공휴일", isHoliday: true),
   ];
 
   test("divide_year", () {
@@ -50,23 +31,12 @@ void main() {
   });
 
   test("weekend", () {
-    var withOutWeekend =
-        mockList.divideByYearAndMonth()[202301]?.toWithoutWeekend();
+    var withOutWeekend = mockList.divideByYearAndMonth()[202301]?.toWithoutWeekend();
     expect(2, withOutWeekend?.length);
   });
 
   test("remaining", () {
-    var withOutWeekend =
-        mockList.divideByYearAndMonth()[202301]?.toRemainingList();
-    expect(4, withOutWeekend?.length);
-  });
-
-  test("toDateTIme", () {
-    var temp = Holiday(
-        date: DateTime(2023, 1, 20),
-        dateName: "test",
-        dateKind: "test",
-        isHoliday: true);
-    expect(temp.toDatetime(), DateTime(2023, 1, 20));
+    var withOutWeekend = mockList.divideByYearAndMonth()[202301]?.toRemainingList();
+    expect(0, withOutWeekend?.length);
   });
 }
