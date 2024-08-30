1f4836d526c8419a11fc066e91ff35b3bb67efe3
bowon lee <asdfg3678@gmail.com>
2024-08-30 19:59:40 +0900

    연간 정보 페이지 UI 개선

--- lib/layout/yearly/yealy_info_view.dart
+++ lib/layout/yearly/yealy_info_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:holiday/client/test_client.dart';
 import 'package:holiday/layout/component/consecutive_holidays_list.dart';
 import 'package:holiday/model/consecutive_holidays/consecutive_holidays_extention.dart';
 import 'package:holiday/model/event_date/event_date_extension.dart';
@@ -29,8 +31,20 @@ class _YearlyInfoViewState extends State<YearlyInfoView> {
   DateTime focusDate = DateTime.now();
 
   @override
+  void initState() {
+    super.initState();
+    test();
+  }
+
+  test() async {
+    final client = GetIt.instance.get<TestClient>();
+    final temp = await client.getTest();
+    print(temp);
+    Logger().e(temp);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    Logger().i(focusDate);
     final holidayState = context.read<HolidayBloc>().state;
 
     if (holidayState is! HolidayBlocLoaded) {
@@ -39,7 +53,14 @@ class _YearlyInfoViewState extends State<YearlyInfoView> {
       );
     }
 
-    final holidayMapByYear = holidayState.holidayList.divideByYear();
+    final holidayList = holidayState.holidayList;
+    final holidayMapByYear = holidayList.divideByYear();
+    final consecutiveHolidaysByCurrentYear =
+        holidayMapByYear[currentYear]!.toEventDateList().toConsecutiveHolidaysList();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToIndex(consecutiveHolidaysByCurrentYear.getUpcomingIndex());
+    });
 
     return Padding(
       padding: const EdgeInsets.all(16.0),
@@ -52,6 +73,11 @@ class _YearlyInfoViewState extends State<YearlyInfoView> {
             yearButtonCallback: (year) {
               setState(() {
                 currentYear = year;
+                if (currentYear == DateTime.now().year) {
+                  focusDate = DateTime.now();
+                } else {
+                  focusDate = DateTime(currentYear, 1, 1);
+                }
               });
             },
           ),
@@ -70,39 +96,26 @@ class _YearlyInfoViewState extends State<YearlyInfoView> {
               ],
             ),
           ),
-          Padding(
-              padding: const EdgeInsets.all(20),
-              child: CustomCalandar(
-                startYear: holidayMapByYear.keys.first,
-                endYear: holidayMapByYear.keys.last,
-                onClickEventDateItem: (datetime) {
-                  Logger().i(datetime);
-                  final temp = holidayMapByYear[currentYear]!
-                      .toEventDateList()
-                      .toConsecutiveHolidaysList()
-                      .findIndexByDatetime(datetime);
-
-                  Logger().i(temp);
-                  _scrollToIndex(temp);
-                },
-                onClickTodayButton: () {
-                  setState(() {
-                    focusDate = DateTime.now();
-                    currentYear = DateTime.now().year;
-                  });
-                },
-                focusDate: focusDate,
-                eventDateList: holidayMapByYear[currentYear]!
-                    .toEventDateList()
-                    .toConsecutiveHolidaysList()
-                    .toEventDateList(),
-              )),
+          CustomCalandar(
+            startYear: currentYear,
+            endYear: currentYear,
+            onClickEventDateItem: (datetime) {
+              _scrollToIndex(consecutiveHolidaysByCurrentYear.findIndexByDatetime(datetime));
+            },
+            onClickTodayButton: () {
+              setState(() {
+                currentYear = DateTime.now().year;
+                focusDate = DateTime.now();
+              });
+            },
+            focusDate: focusDate,
+            eventDateList: consecutiveHolidaysByCurrentYear.toEventDateList(),
+          ),
           Expanded(
             // height: 30,
             child: ConsecutiveHolidaysListComponent(
                 onTapItem: (item) {
                   setState(() {
-                    // 시작년의 최초일보다 작으면
                     if (holidayMapByYear.keys.first > item.dateList.first.datetime.year) {
                       focusDate = item.dateList.last.datetime;
                     } else {
@@ -119,15 +132,11 @@ class _YearlyInfoViewState extends State<YearlyInfoView> {
     );
   }
 
-  /**
-   * 특정 월을 기준으로 가장 가까움 아이템을 찾아주는 항목이 필요하다.
-   * 달력을 해당 연휴의 시작점이 있는 월로 이동시키는 항목이 필요하다.
-   */
   void _scrollToIndex(int index) {
     final position = _listScrollController.position.minScrollExtent + (index * 80.0);
     _listScrollController.animateTo(
       position,
-      duration: const Duration(milliseconds: 100),
+      duration: const Duration(milliseconds: 200),
       curve: Curves.easeInOut,
     );
   }
